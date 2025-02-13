f31366d52900826f80cbc4a85415e48d1efbcaed
killlercam <herosviral@gmail.com>
2025-02-13 22:34:34 +0530

    Some changes with the utilities of tailwindcss

--- UI/email-sender/src/components/EmailSender.jsx
+++ UI/email-sender/src/components/EmailSender.jsx
@@ -1,9 +1,8 @@
 // eslint-disable-next-line no-unused-vars
-import React, { useState } from "react";
+import React, { useState,useRef } from "react";
 import toast from "react-hot-toast";
 import { sendEmail } from "../services/email.service";
-// import validator from 'validator';
-
+import { Editor } from '@tinymce/tinymce-react';
  function EmailSender() {
   // connecting with api
   const [emailData, setEmailData] = useState({
@@ -13,6 +12,9 @@ import { sendEmail } from "../services/email.service";
   });
   //for loader
   const [sending,setSending]=useState(false);
+
+  //For the text editor
+  const editorRef = useRef(null);
 
   //This will track the name entered and change the name accordingly
    function handleFieldChange(event, name) {
@@ -38,11 +40,16 @@ import { sendEmail } from "../services/email.service";
       setSending(true);
       await sendEmail(emailData);
       toast.success("Email Send Successfully!!");
-      setEmailData({
+      setEmailData(() => ({
         to: "",
         subject: "",
         message: "",
-      });
+      }));
+      // document.querySelector("input[name='to']").value = "";
+      // document.querySelector("input[name='subject']").value = "";
+      // if (editorRef.current) {
+      //   editorRef.current.setContent("");
+      // }
     }catch(error){
       console.log(error);
       toast.error("Email Not Sent !!");
@@ -52,10 +59,9 @@ import { sendEmail } from "../services/email.service";
     }
   }
 
-
   return (
     <div className="w-full min-h-screen flex justify-center items-center">
-      <div className="email_card bg-white -mt-10 w-1/3 rounded-2xl p-4 shadow-2xl">
+      <div className="email_card bg-white -mt-10 w-1/2 rounded-2xl p-4 shadow-2xl">
         <h1 className="text-3xl text-gray-800 p-0.5">Email Sender</h1>
         <p className="text-gray-700">
           Send Email&apos;s to any person with your own app...
@@ -95,6 +101,30 @@ import { sendEmail } from "../services/email.service";
               rows={5}
               className="textarea textarea-bordered textarea-lg w-full "
             ></textarea>
+
+            {/* <Editor
+            onEditorChange={()=>{
+              setEmailData({...emailData,'message':editorRef.current.getContent()})
+            }}
+             onInit={(evt, editor) => editorRef.current = editor}
+             apiKey="7y09jibczu6vcialnxt4gup840zh63brxru0w5jm7ofmm30i"
+             init={{
+              plugins: [
+                // Core editing features
+                'anchor', 'autolink', 'charmap', 'codesample', 'emoticons', 'image', 'link', 'lists', 'media', 'searchreplace', 'table', 'visualblocks', 'wordcount',
+                'checklist', 'mediaembed', 'casechange', 'export', 'formatpainter', 'pageembed', 'a11ychecker', 'tinymcespellchecker', 'permanentpen', 'powerpaste', 'advtable', 'advcode', 'editimage', 'advtemplate', 'ai', 'mentions', 'tinycomments', 'tableofcontents', 'footnotes', 'mergetags', 'autocorrect', 'typography', 'inlinecss', 'markdown','importword', 'exportword', 'exportpdf'
+              ],
+              toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+              tinycomments_mode: 'embedded',
+              tinycomments_author: 'Author name',
+              mergetags_list: [
+                { value: 'First.Name', title: 'First Name' },
+                { value: 'Email', title: 'Email' },
+              ],
+              ai_request: (request, respondWith) => respondWith.string(() => Promise.reject('See docs to implement AI Assistant')),
+            }}
+            /> */}
+            
           </div>
 
           {/* Loading component */}
