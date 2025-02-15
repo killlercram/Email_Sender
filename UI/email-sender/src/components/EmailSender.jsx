1a6b9ba19ae56b21b8369bf7d77756c25411edd6
killlercam <herosviral@gmail.com>
2025-02-15 18:22:44 +0530

    Added the the Rich text editor Using Jodit which is great,it works perfectly fine and easy to use.This finally Completes the Ui Part.

--- UI/email-sender/src/components/EmailSender.jsx
+++ UI/email-sender/src/components/EmailSender.jsx
@@ -1,9 +1,10 @@
 // eslint-disable-next-line no-unused-vars
-import React, { useState,useRef } from "react";
+import React, { useState, useRef } from "react";
 import toast from "react-hot-toast";
 import { sendEmail } from "../services/email.service";
-import { Editor } from '@tinymce/tinymce-react';
- function EmailSender() {
+import JoditEditor from "jodit-react";
+
+function EmailSender() {
   // connecting with api
   const [emailData, setEmailData] = useState({
     to: "",
@@ -11,32 +12,35 @@ import { Editor } from '@tinymce/tinymce-react';
     message: "",
   });
   //for loader
-  const [sending,setSending]=useState(false);
+  const [sending, setSending] = useState(false);
 
-  //For the text editor
-  const editorRef = useRef(null);
+  //for rich editor
+  const editor = useRef(null);
+  const config = {
+    readonly: false, // Make it editable
+    height: 300,
+    toolbarButtonSize: "large",
+  };
 
   //This will track the name entered and change the name accordingly
-   function handleFieldChange(event, name) {
+  function handleFieldChange(event, name) {
     setEmailData({ ...emailData, [name]: event.target.value });
+
   }
 
   async function handleSubmit(event) {
     event.preventDefault(); //stopping the default action
-    // console.log(emailData);
-    // const email = emailData.to;
-    // if(validator.isEmail(email)){
-    //   return "Valid Email";
-    // }else{
-    //   return "Enter Valid Email;";
-    // }
-    if(emailData.to==''||emailData.subject==''||emailData.message==''){
+    if (
+      emailData.to == "" ||
+      emailData.subject == "" ||
+      emailData.message == ""
+    ) {
       toast.error("Invalid Input!!");
       return;
     }
 
     //sending Email using api
-    try{
+    try {
       setSending(true);
       await sendEmail(emailData);
       toast.success("Email Send Successfully!!");
@@ -45,16 +49,10 @@ import { Editor } from '@tinymce/tinymce-react';
         subject: "",
         message: "",
       }));
-      // document.querySelector("input[name='to']").value = "";
-      // document.querySelector("input[name='subject']").value = "";
-      // if (editorRef.current) {
-      //   editorRef.current.setContent("");
-      // }
-    }catch(error){
+    } catch (error) {
       console.log(error);
       toast.error("Email Not Sent !!");
-    }
-    finally{
+    } finally {
       setSending(false);
     }
   }
@@ -67,8 +65,8 @@ import { Editor } from '@tinymce/tinymce-react';
           Send Email&apos;s to any person with your own app...
         </p>
         <form action="" onSubmit={handleSubmit}>
-          {/* For to */}
-          <div className="input_field mt-4 w-full">
+          {/* For to  */}
+           <div className="input_field mt-4 w-full">
             <label className="block mb-0.5">To:</label>
             <input
               type="email"
@@ -94,57 +92,50 @@ import { Editor } from '@tinymce/tinymce-react';
           {/* For Text Area */}
           <div className="input_message mt-4">
             <label className="block mb-0.5">Your Message:</label>
-            <textarea
+            {/* <textarea
               value={emailData.message}
               onChange={(event) => handleFieldChange(event, "message")}
               placeholder="Write your thoughts here..."
               rows={5}
               className="textarea textarea-bordered textarea-lg w-full "
-            ></textarea>
+            ></textarea> */}
 
-            {/* <Editor
-            onEditorChange={()=>{
-              setEmailData({...emailData,'message':editorRef.current.getContent()})
-            }}
-             onInit={(evt, editor) => editorRef.current = editor}
-             apiKey="7y09jibczu6vcialnxt4gup840zh63brxru0w5jm7ofmm30i"
-             init={{
-              plugins: [
-                // Core editing features
-                'anchor', 'autolink', 'charmap', 'codesample', 'emoticons', 'image', 'link', 'lists', 'media', 'searchreplace', 'table', 'visualblocks', 'wordcount',
-                'checklist', 'mediaembed', 'casechange', 'export', 'formatpainter', 'pageembed', 'a11ychecker', 'tinymcespellchecker', 'permanentpen', 'powerpaste', 'advtable', 'advcode', 'editimage', 'advtemplate', 'ai', 'mentions', 'tinycomments', 'tableofcontents', 'footnotes', 'mergetags', 'autocorrect', 'typography', 'inlinecss', 'markdown','importword', 'exportword', 'exportpdf'
-              ],
-              toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
-              tinycomments_mode: 'embedded',
-              tinycomments_author: 'Author name',
-              mergetags_list: [
-                { value: 'First.Name', title: 'First Name' },
-                { value: 'Email', title: 'Email' },
-              ],
-              ai_request: (request, respondWith) => respondWith.string(() => Promise.reject('See docs to implement AI Assistant')),
-            }}
-            /> */}
-            
+            <JoditEditor
+              ref={editor}
+              value={emailData.message}
+              config={config}
+              tabIndex={1} // tabIndex of textarea
+              onBlur={(setContent)=>{
+                setEmailData((prev)=>({ ...prev,message:setContent
+                }));
+              }}
+            />
           </div>
 
           {/* Loading component */}
-          {sending && (<div className="loader flex justify-center flex-col items-center mt-3">
-          <span className="loading loading-dots loading-md"></span>
-          sending email . . .
-          </div>)}
+          {sending && (
+            <div className="loader flex justify-center flex-col items-center mt-3">
+              <span className="loading loading-dots loading-md"></span>
+              sending email . . .
+            </div>
+          )}
 
           {/* for buttons */}
           <div className="buttons mt-4 flex justify-center gap-2">
             <button
-              disabled={sending}//setting to disable while sending
+              disabled={sending} //setting to disable while sending
               type="submit"
               className="btn btn-active bg-blue-600 hover:bg-blue-800 text-white"
             >
               Send Email
             </button>
-            <button type="button" onClick={()=>{setEmailData({to: "",
-        subject: "",
-        message: ""})}} className="btn btn-active bg-red-600 hover:bg-red-700 text-white">
+            <button
+              type="button"
+              onClick={() => {
+                setEmailData({ to: "", subject: "", message: "" });
+              }}
+              className="btn btn-active bg-red-600 hover:bg-red-700 text-white"
+            >
               Clear
             </button>
           </div>
